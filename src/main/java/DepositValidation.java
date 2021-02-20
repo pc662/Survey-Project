2aad7524cd3caf92930272f18a71981fb0b0ae36
Peter <pc662@drexel.edu>
2021-02-20 13:08:57 -0500

    Created DepositValidation and DepositValidationTest

---
+++ src/main/java/DepositValidation.java
@@ -0,0 +1,2 @@
+public class DepositValidation {
+}
---
+++ src/test/java/DepositValidationTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DepositValidationTest {
+
+    CreateValidation validation;
+    Bank bank = new Bank();
+    Checking checking = new Checking("01234567", 0.01);
+    Saving saving = new Saving("01234567", 0.01);
+
+
+    @Test
+    void command_validate_correct_deposit() {
+        validation = verify("Deposit 01234567 1000");
+        assertTrue(validation.validate());
+    }
+
+    private CreateValidation verify(String s) {
+        return new DepositValidation(s, bank);
+    }
+
+}
