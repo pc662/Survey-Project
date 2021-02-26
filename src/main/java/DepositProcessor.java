1a33c1297298e3d3a373c3e02eb63379beb56a27
Peter <pc662@drexel.edu>
2021-02-26 17:23:31 -0500

    Finished testing the DepositProcessor

--- src/main/java/DepositProcessor.java
+++ src/main/java/DepositProcessor.java
@@ -1,7 +1,18 @@
 public class DepositProcessor extends CommandProcessor {
 
+    private String accountID;
+    private double depositAmount;
+
     DepositProcessor(String command, Bank bank) {
         super(command, bank);
+        accountID = parsedCommand[1];
+        depositAmount = Double.parseDouble(parsedCommand[2]);
     }
+
+    @Override
+    public void process() {
+        bank.getAccount(accountID).deposit(depositAmount);
+    }
+
 
 }
