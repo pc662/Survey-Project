7f12fe39a785b55295a773d16ce51e50191570e1
Peter <pc662@drexel.edu>
2021-02-26 16:08:06 -0500

    Refactored into some methods

--- src/test/java/CommandProcessorTest.java
+++ src/test/java/CommandProcessorTest.java
@@ -4,14 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandProcessorTest {
     String checkingType = "checking";
+    String checkingAccountCommand = "create checking 01234567 1.0";
+    String checkingID = "01234567";
+    Bank bank = new Bank();
+
 
     @Test
     void create_checking() {
-        Bank bank = new Bank();
-        String checkingAccountCommand = "create checking 01234567 1.0";
+        process(bank, checkingAccountCommand);
+        assertEquals(getAccount(checkingID).getAccountType(), checkingType);
+    }
+
+
+    private Account getAccount(String string) {
+        return bank.getAccount(string);
+    }
+
+    private void process(Bank bank, String checkingAccountCommand) {
         CommandProcessor commandProcessor = new CommandProcessor(checkingAccountCommand, bank);
         commandProcessor.process();
-        assertEquals(bank.getAccount("01234567").getAccountType(), checkingType);
     }
 
 
