54c5a7766c41a2b8f477b2c150f2ebc64600c1af
Peter <pc662@drexel.edu>
2021-02-26 20:43:38 -0500

    Finished the two typo tests

--- src/main/java/MasterControl.java
+++ src/main/java/MasterControl.java
@@ -1,2 +1,22 @@
+import java.util.List;
+
 public class MasterControl {
+    Bank bank;
+    Validation commandValidator;
+    CommandProcessor commandProcessor;
+    CommandStorage commandStorage;
+
+    public MasterControl(Bank bank, Validation commandValidator, CommandProcessor commandProcessor,
+                         CommandStorage commandStorage) {
+        this.bank = bank;
+        this.commandValidator = commandValidator;
+        this.commandProcessor = commandProcessor;
+        this.commandStorage = commandStorage;
+    }
+
+
+    public List<String> start(List<String> input) {
+        commandStorage.storeInvalidCommand(input.get(0));
+        return commandStorage.getInvalidCommands();
+    }
 }
--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -1,11 +1,51 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MasterControlTest {
 
     MasterControl masterControl;
+    List<String> input;
 
     @BeforeEach
     void setUp() {
+        input = new ArrayList<>();
+        Bank bank = new Bank();
+        masterControl = new MasterControl(bank, new Validation(bank), new CommandProcessor(bank),
+                new CommandStorage());
+
+    }
+
+    @Test
+    void typo_in_create_command_is_invalid() {
+        input.add("creat checking 12345678 1.0");
+
+        List<String> actual = masterControl.start(input);
+        assertSingleCommand(actual, "creat checking 12345678 1.0");
+    }
+
+
+    @Test
+    void typo_in_deposit_command_is_invalid() {
+        input.add("depositt 12345678 100");
+
+        List<String> actual = masterControl.start(input);
+        assertSingleCommand(actual, "depositt 12345678 100");
+    }
+
+    @Test
+    void two_typo_commands_both_invalid() {
+        input.add("creat checking 12345678 1.0");
+        input.add("depositt 12345678 100");
+    }
+
+    private void assertSingleCommand(List<String> actual, String s) {
+        assertEquals(1, actual.size());
+        assertEquals(s, actual.get(0));
     }
 
 }
