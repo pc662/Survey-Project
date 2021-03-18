b5bb9d1c9a07f0940c3213cb0af396ecb1e54c69
Peter <pc662@drexel.edu>
2021-03-18 16:16:09 -0400

    Added the PassTime validation class

---
+++ src/main/java/banking/PassTimeValidation.java
@@ -0,0 +1,10 @@
+package banking;
+
+public class PassTimeValidation {
+    public PassTimeValidation(Bank bank) {
+    }
+
+    public boolean validate(String originalString) {
+        return true;
+    }
+}
