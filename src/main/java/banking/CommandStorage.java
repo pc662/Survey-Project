2a14050731bbea43420b427c8401dfb154f123e3
Peter <pc662@drexel.edu>
2021-03-20 02:22:15 -0400

    Added a Map for storing lists of commands per account.

--- src/main/java/banking/CommandStorage.java
+++ src/main/java/banking/CommandStorage.java
@@ -2,14 +2,20 @@ package banking;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CommandStorage {
 
     ArrayList<String> invalidCommands = new ArrayList<>();
+    Map<String, List<String>> validCommands;
 
 
     public void storeInvalidCommand(String string) {
         invalidCommands.add(string);
+    }
+
+    public void storeValidCommand(String accountID, String command) {
+        validCommands.get(accountID).add(command);
     }
 
     public List<String> getInvalidCommands() {
@@ -17,4 +23,7 @@ public class CommandStorage {
     }
 
 
+    public void createHistory(String accountID) {
+        validCommands.put(accountID, new ArrayList<>());
+    }
 }
--- src/main/java/banking/MasterControl.java
+++ src/main/java/banking/MasterControl.java
@@ -7,6 +7,7 @@ public class MasterControl {
     Validation commandValidator;
     CommandProcessor commandProcessor;
     CommandStorage commandStorage;
+    String[] parsedCommand;
 
     public MasterControl(Bank bank, Validation commandValidator, CommandProcessor commandProcessor,
                          CommandStorage commandStorage) {
@@ -14,17 +15,28 @@ public class MasterControl {
         this.commandValidator = commandValidator;
         this.commandProcessor = commandProcessor;
         this.commandStorage = commandStorage;
+
     }
 
 
     public List<String> start(List<String> input) {
         for (String command : input) {
             if (commandValidator.validate(command)) {
-                commandProcessor.process(command);
+                parsedCommand = command.split(" ", 0);
+                checkCreate(parsedCommand[0]);
             } else {
                 commandStorage.storeInvalidCommand(command);
             }
         }
         return commandStorage.getInvalidCommands();
     }
+
+    private void checkCreate(String command) {
+        if (command.equalsIgnoreCase("create")) {
+            commandStorage.createHistory();
+        } else if (command.equalsIgnoreCase("deposit")) {
+            commandStorage.storeValidCommand();
+        }
+    }
+}
 }
